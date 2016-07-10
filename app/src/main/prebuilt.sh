5835c5bdbad53267f0758d7ea683143ef1708c2f
nian0114 <268078545@qq.com>
2016-07-10 08:20:35 +0800

    修复部分网站解析失败

--- app/src/main/prebuilt.sh
+++ app/src/main/prebuilt.sh
@@ -9,7 +9,7 @@ cat winhelp | sed -e 's/\r//g' >> new
 cat adaway | sed -e 's/\r//g' >> new
 cat hosts-file | sed -e 's/\r//g' >> new
 cat nian-host | awk '{printf("127.0.0.1 %s\n",$1)}' | sed -e 's/\r//g' >> new
-cat new | sed '/#/d' | sed '/@/d' | gawk '!a[$2]++' |sed '/localhost/d' | gawk '{printf("127.0.0.1 %s\n",$2)}' |sort > final
+cat new | sed '/#/d' | sed '/@/d' | gawk '!a[$2]++' | sed '/localhost/d' | sed '/ip6-allnodes/d' | sed '/ip6-allrouters/d' | gawk '{printf("127.0.0.1 %s\n",$2)}' |sort > final
 split -a 1 -l 5000 final adblock
 rm -rf final new hosts-file adaway winhelp cn nian-host
 mv adblock* assets/
